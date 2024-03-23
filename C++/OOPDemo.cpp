99ddf83619863402df1513e642f34bcd8de67b59
Yamuna_constructions <bagaltanmay1@gmail.com>
2024-03-23 20:14:02 +0530

    23/03/2024

--- C++/OOPDemo.cpp
+++ C++/OOPDemo.cpp
@@ -23,15 +23,31 @@ class Arithematic
         Ans = No1 + No2;
         return Ans; 
     }
-    ~Arithematic()
+    int Substraction()
     {
-        cout<<"Inside Destructor\n";
+        int Ans = 0;
+        Ans = No1 - No2;
+        return Ans;
     }
 };
 
 int main()
 {
+    int Value1 = 0, Value2 = 0, Ret = 0;
+    
+    cout<<"Enter first number : \n";
+    cin>>Value1;
 
+    cout<<"Enter Second number : \n";
+    cin>>Value2;
 
+    Arithematic obj (Value1, Value2);
+
+    Ret = obj.Addition();
+    cout<<"Addition is : "<<Ret<<"\n";
+
+    Ret = obj.Substraction();
+    cout<<"Substraction is : "<<Ret<<"\n";
+    
     return 0;
 }
