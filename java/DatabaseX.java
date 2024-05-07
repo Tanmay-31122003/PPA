174952097a5a87c611616551c661f5ade116f418
Yamuna_constructions <bagaltanmay1@gmail.com>
2024-05-07 22:16:25 +0530

    05/05/2024

--- java/DatabaseX.java
+++ java/DatabaseX.java
@@ -1,11 +1,15 @@
 import java.sql.*;          // 1
 
-class Database
+class DatabaseX
 {
     public static void main(String arg[]) throws Exception
     {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            
             Connection cobj = DriverManager.getConnection("jdbc:mysql://localhost:3306/ppa50","root","root");   // 2
-        
+            
             Statement sobj = cobj.createStatement();        // 3
 
             ResultSet robj = sobj.executeQuery("select * from student");      // 4
@@ -15,6 +19,10 @@ class Database
                 System.out.println("RNO : "+robj.getInt("rno"));
                 System.out.println("Name : "+robj.getString("name"));
                 System.out.println("City : "+robj.getString("City"));
-            }    
+            }
+        }
+        catch(Exception obj)
+        {}
     }
+
 }
